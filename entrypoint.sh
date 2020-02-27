353dce73e6150f3f18d1694b571bb422c8c70eb7
Adrián Sosa <56990010+AdrianSosaUV@users.noreply.github.com>
2020-02-26 21:53:27 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,7 @@ git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 if [ -z "$(git status --porcelain)" ]; then
-    echo "Nothing to commit" && \
-    exit 0
+    echo "Nothing to commit"
 fi && \
 git add . && \
 git commit -m 'Deploy to GitHub Pages' && \
